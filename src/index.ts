66a1e4710ca4eb2c19b54a1a5033f42d4916c6d6
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2024-12-15 13:16:04 -0800

    migrated to typescript

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import "dotenv/config"
+// Initialize the Express app
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// // to prevent attackers from knowing the type of technology user
+app.disable('x-powered-by');
+
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+// Start the server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
