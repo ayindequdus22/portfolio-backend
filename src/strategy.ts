fb6be89cc9ec73e72a8bd49269c0e8c87ecda976
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2024-12-19 14:51:47 -0800

    passport added

--- src/strategy.ts
+++ src/strategy.ts
@@ -1,15 +1,47 @@
 import passport from "passport";
-import {Strategy as LocalStrategy} from "passport-local";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcryptjs"
 import { client } from "./connectDb";
+import logger from "./utils/logger";
+
+
 passport.use(new LocalStrategy(
     // options
-    {usernameField:'email',passwordField:'password'},
+    { usernameField: 'email', passwordField: 'password' },
     // verify fn
-    async function verify(email:string,password:string,done:any){
-        client.query("Select * from users where email=$1",[email],).then((err,user)=>{
-            if (err) { return done(err); }
-            if (!user) { return done(null, false, { message: 'Incorrect username or password.' }); }
-            
-        })
+    async function verify(email: string, password: string, done: any) {
+        try {
+            const result = await client.query("Select * from users where email=$1", [email],);
+            if (result.rows.length === 0) {
+                return done(null, false, { message: 'Incorrect username or password.' });
+            }
+            const user = result.rows[0];
+            logger.debug(`${result.rows},user ${user}`,);
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (isMatch) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'Password incorrect' });
+            }
+        } catch (error) {
+            return done(error, null);
+        }
+
     }));
-    // C:\Users\abdul\Documents\portfolio\portfolio-backend\node_modules\passport-local\lib\strategy.js
+passport.serializeUser((user, done) => {
+    done(null, user.id);  // (user as { id: number }).id Use type assertion to say that 'user' has an 'id'
+});
+passport.deserializeUser(async (id: number, done) => {
+    try {
+        const result = await client.query("Select * from users where id=$1", [id],);
+        if (result.rows.length === 0) {
+            return done(null, false);
+        }
+        const user = result.rows[0];
+        done(null, user);
+    } catch (err) {
+        done(err, null);
+    }
+})
+// C:\Users\abdul\Documents\portfolio\portfolio-backend\node_modules\passport-local\lib\strategy.js
