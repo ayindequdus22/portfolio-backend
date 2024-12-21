8a8172eb03d5a59fd0515205638273be8a97579b
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2024-12-20 16:41:33 -0800

    finna deploy

--- portfolio-admin/tailwind.config.js
+++ portfolio-admin/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      }, 
+      screens: {
+        'max-mdLap': { 'max': '996px' },
+        'max-tab': { 'max': '778px' },
+        'max-mdPhone': { 'max': '550px' },
+        'max-phone': { 'max': '426px' },
+      }
+    },
   },
   plugins: [],
 }
