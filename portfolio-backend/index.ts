e1a350c9e1e38acb81a98e7ecfee07239b28818c
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2025-02-06 02:41:37 -0800

    another dey cookie issue

--- portfolio-backend/index.ts
+++ portfolio-backend/index.ts
@@ -44,8 +44,8 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 24, // 24 hour
     httpOnly: true,		
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: process.env.NODE_ENV  === 'production' ? 'none' :'lax'
+    secure: process.env.NODE_ENV === "production", // HTTPS only in production
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
   },
   store: new pgSession({ pool: client })
 }));
